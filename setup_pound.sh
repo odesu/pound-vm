59cfa0873f19d6dbcdb1a4e6db63ff54a8de6d90
odesu <thimo.hoerster@odesu.de>
2018-01-04 20:56:36 +0100

    added simple pound config

--- setup_pound.sh
+++ setup_pound.sh
@@ -3,5 +3,10 @@
 sudo -s
 apt update
 apt -y install htop nano pound
+
 echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 /etc/init.d/ssh restart
+
+cp -f /vagrant/pound /etc/default
+cp -f /vagrant/pound.cfg /etc/pound
+/etc/init.d/pound restart
